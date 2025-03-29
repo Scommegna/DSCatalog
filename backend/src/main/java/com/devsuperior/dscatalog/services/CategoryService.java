f94d2e7ec7d2db724c0235982af1e1c6ba63ce05
Lucas Scommegna <scommegnal@hotmail.com>
2025-03-29 15:06:16 -0300

    first commit

---
+++ backend/src/main/java/com/devsuperior/dscatalog/services/CategoryService.java
@@ -0,0 +1,26 @@
+package com.devsuperior.dscatalog.services;
+
+import com.devsuperior.dscatalog.dto.CategoryDTO;
+import com.devsuperior.dscatalog.entities.Category;
+import com.devsuperior.dscatalog.repositories.CategoryRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+
+@Service
+public class CategoryService {
+
+    @Autowired
+    private CategoryRepository repository;
+
+    @Transactional(readOnly = true)
+    public List<CategoryDTO> findAll() {
+        List<Category> list = repository.findAll();
+
+        List<CategoryDTO> listDTO = list.stream().map(category -> new CategoryDTO(category)).toList();
+
+        return listDTO;
+    }
+}
