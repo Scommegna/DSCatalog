9e1fa9e7058fdc0e329629745d8d90204dcb0d2b
Lucas Scommegna <scommegnal@hotmail.com>
2025-03-31 08:49:05 -0300

    delete category route

--- backend/src/main/java/com/devsuperior/dscatalog/resources/CategoryResource.java
+++ backend/src/main/java/com/devsuperior/dscatalog/resources/CategoryResource.java
@@ -47,4 +47,11 @@ public class CategoryResource {
 
         return ResponseEntity.ok(dto);
     }
+
+    @DeleteMapping(value = "/{id}")
+    public ResponseEntity<Void> delete(@PathVariable Long id) {
+        service.delete(id);
+
+        return ResponseEntity.noContent().build();
+    }
 }
